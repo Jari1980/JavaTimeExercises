b736e42b71dba059196cc3d3213c5dd2e810a7e6
Jari Tenhunen <jari.tenhunen@outlook.com>
2024-10-11 12:57:13 +0200

    Initial

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        var scan = new Scanner(System.in);
+        int num;
+        while (true) {
+            System.out.println("Which exercise do you want to see, 1-11 (111 for the duplicate exercise 1), :");
+            try {
+                num = scan.nextInt();
+                switch (num) {
+                    case 1:
+                        exercise1();
+                        break;
+                    default:
+                        System.out.println("There is no such exercise");
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println("Closing program");
+                System.exit(0);
+            }
+        }
+    }
+    public static void exercise1(){
+
+    }
+}
